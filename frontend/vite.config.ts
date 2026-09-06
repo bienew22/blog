80f5d89f98926e255f23b9b2ccca12072b28812b
BieNew22 <newbie11004@gmail.com>
2026-09-06 17:56:15 +0900

    feat: tailwindcss 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss()
   ],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['frontend', 'localhost'],
+    allowedHosts: ['frontend', 'localhost', '.trycloudflare.com'],
   },
   resolve: {
     alias: {
