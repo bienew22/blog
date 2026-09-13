ede95dd11e82d249c913b9f90c411a34f8603251
BieNew22 <newbie11004@gmail.com>
2026-09-13 21:02:49 +0900

    feat: icon 추가

--- frontend/src/components/icon/index.ts
+++ frontend/src/components/icon/index.ts
@@ -2,8 +2,14 @@
 
 import CategoryIcon from './CategoryIcon.vue'
 import PostIcon from './PostIcon.vue'
+import LightThemeIcon from './LightThemeIcon.vue'
+import DarkThemeIcon from './DarkThemeIcon.vue'
+import GithubIcon from './GithubIcon.vue'
 
 export {
     CategoryIcon,
     PostIcon,
+    LightThemeIcon,
+    DarkThemeIcon,
+    GithubIcon,
 }
